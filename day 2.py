557674102cada68fe08b23a2603ffb76a223bc55
Kennedy Wambugu <irungukennedy003@gmail.com>
2025-03-12 12:56:25 +0300

    Introduction to Python

---
+++ day 2.py
@@ -0,0 +1,33 @@
+age = 25
+height = 5.75
+name = "Kennedy"
+is_student = True
+
+print ("Name: ",name)
+print ("Age: ",age)
+print ("Height: ",height)
+print ("Is a student?: ",is_student)
+#check data types
+print ()
+print (type(age))
+print (type(height))
+print (type(name))
+print (type(is_student))
+print ()
+#dynamic typing
+age = 20
+print (age)
+age = "Now I Am Twenty"
+print (age)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ temperature.py
@@ -0,0 +1,33 @@
+temperature = 20
+if temperature >25:
+  print ("Hot Day")
+else:
+  print ("Cool Day")
+print ()
+# Control flow example for grading student marks
+# This will ask the user for input and grade the student's performance
+
+# Prompt the user for student marks
+marks = int(input("Enter the student's marks (0-100): "))
+
+# Grading system based on the marks
+if marks > 70:
+    print("Grade: Distinction 🏆")
+elif marks >= 60:
+    print("Grade: Credit 🎖️")
+elif marks >= 50:
+    print("Grade: Pass 👍")
+else:
+    print("Grade: Fail ❌")
+print ()
+def isnumberinrange (num,i=25,n=100)
+  if num in range (i,n):
+     print("found")
+  else:
+     print ("not found")
+isnumberinrange (20)
+isnumberinrange (20,0)
+isnumberinrange (20,0,10)
+
+
+
